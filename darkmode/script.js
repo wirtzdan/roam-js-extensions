e40f69e45c7b8210454b0bf6e86408b489ebd30e
Daniel Wirtz <danielwirtzx@gmail.com>
2020-09-28 22:07:59 +0200

    :pencil: Add better description

--- darkmode/script.js
+++ darkmode/script.js
@@ -1,16 +1,20 @@
 // ---- VARIABLES ---- //
 // Changes this variables to adjust the appearance of the dark mode
 // Tip: The default is 100, from there you can increase or decrease the value
-window.darkmode = {
+window.roamdarkmode = {
   brightness: "100",
   contrast: "90",
   sepia: "10",
 };
 
 // ---- LOAD MAIN SCRIPT ---- //
-// Link to the repository:
+// Link to the repository: https://github.com/wirtzdan/roam-js-extensions
 const script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://github.com/wirtzdan/";
+script.src =
+  "https://cdn.jsdelivr.net/gh/wirtzdan/roam-js-extensions@master/darkmode/index.js";
 script.async = true;
 document.getElementsByTagName("head")[0].appendChild(script);
+
+// ---- QUESTIONS ---- //
+// For questions or feedback reach out to me on Twitter @wirtzdan
